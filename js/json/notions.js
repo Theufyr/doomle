cf1a799a054e08bfbef5f26104f680a11209e64c
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-10 14:05:03 +0100

    update localStorage

--- js/json/notions.js
+++ js/json/notions.js
@@ -9,6 +9,7 @@ export default notionList = {
 	"protocol": ["Protocole Web",   "",		false],
 	"git":      ["Git",             "",		false],
 	"github":   ["GitHub",          "",     false],
+	"storage":  ["localStorage",    "",     false],
 	"cookie":   ["Cookies",         "",     false],
 	"access":   ["Accessibilité",   "",     false],
 	"ai":       ["IA",              "IA",   false]
