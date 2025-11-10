531c21047847262041995c1b6a5917de11955aed
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-10 19:01:37 +0100

    udpate databases

--- js/json/notions.js
+++ js/json/notions.js
@@ -4,13 +4,13 @@ export default notionList = {
 	"css":      ["CSS",             "CSS",  false],
 	"js":       ["Javascript",      "JS",   false],
 	"ts":       ["TypeScript",      "TS",   false],
+	"storage":  ["localStorage",    "",     false],
+	"cookie":   ["Cookies",         "",     false],
 	"markdown": ["MarkDown",        "M↓",   false],
 	"terminal": ["Terminal",        "",		false],
 	"protocol": ["Protocole Web",   "",		false],
 	"git":      ["Git",             "",		false],
 	"github":   ["GitHub",          "",     false],
-	"storage":  ["localStorage",    "",     false],
-	"cookie":   ["Cookies",         "",     false],
 	"access":   ["Accessibilité",   "",     false],
 	"ai":       ["IA",              "IA",   false]
 }
