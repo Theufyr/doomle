c7f689fb7d651e84ac32be0d45d7349ac59e0363
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-15 21:30:50 +0100

    update database.json and notions.js

--- js/json/notions.js
+++ js/json/notions.js
@@ -6,11 +6,12 @@ export default notionList = {
 	"ts":       ["TypeScript",      "TS",   false],
 	"storage":  ["localStorage",    "",     false],
 	"cookie":   ["Cookies",         "",     false],
-	"markdown": ["MarkDown",        "M↓",   false],
+	"api":   	["API",         	"API",  false],
 	"terminal": ["Terminal",        "",		false],
 	"protocol": ["Protocole Web",   "",		false],
 	"git":      ["Git",             "",		false],
 	"github":   ["GitHub",          "",     false],
+	"markdown": ["MarkDown",        "M↓",   false],
 	"access":   ["Accessibilité",   "",     false],
 	"ai":       ["IA",              "IA",   false]
 }
